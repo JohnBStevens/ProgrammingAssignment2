2b3e65c18b76ac659b7dcf924571422d77813739
John Stevens <JohnStevens@JohnStevens.info>
2016-11-07 20:12:10 -0500

    Started over again after reading the Demystifying article.

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,17 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   
-  #generate the matrix
-  mx <- maxtrix(rnorm(16),nrow=10,ncol=10)
-  
-  #return the matrix
-  mx
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
   
 }
 
@@ -17,51 +23,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve returns the inverse of the given matrix, 
 ## calculating the inverse if it is not already available in the cache
 
-cacheSolve <- function(pmx, ...) {
+cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   
-  ##Glossary of Symbols
-  #pmx = provided matrix
-  #cmx = cached matrix
-  #imx = inverted matrix
-  #cimx = cached inverted matrix
-  #rmx = returned matrix
-  
-  #is there a cached matrix?
-      if(exists(cmx)) {
-            
-  #if so, 
-      #does it match the current matrix?
-      if(cmx==x) {
-      #if so, 
-      #return the cached inverse
-      rmx <- cimx
-    }
-    #if not, 
-    else {
-      #set flag
-      needtocalculate == TRUE
-    }
-  }
-  
-  #if not,
-  else {
-      #set flag
-      needtocalculate == TRUE
-      #cache the matrix
-      cmx <- pmx
-  }
-  
-  if (needtocalculate) {
-    
-    #calculate the inverse
-    rmx <- solve(pmx)
-    
-    #cache the inverse
-    cmx <<- rmx
-    
-  }
-  
-  #return the inverse
-  rmx
+      m <- x$getinverse()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
+      
 }
