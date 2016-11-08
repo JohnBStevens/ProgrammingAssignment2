4cdcff9005b92653219bfcba4629695d39f27e41
John Stevens <JohnStevens@JohnStevens.info>
2016-11-07 19:31:37 -0500

    Added more comment logic, including symbol glossary. Completed makeCacheMatrix function with working code.

--- cachematrix.R
+++ cachematrix.R
@@ -8,26 +8,59 @@ makeCacheMatrix <- function(x = matrix()) {
   #generate the matrix
   mx <- maxtrix(rnorm(16),nrow=10,ncol=10)
   
+  #return the matrix
+  mx
+  
 }
 
 
 ## cacheSolve returns the inverse of the given matrix, 
 ## calculating the inverse if it is not already available in the cache
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(pmx, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  ##Glossary of Symbols
+  #pmx = provided matrix
+  #cmx = cached matrix
+  #imx = inverted matrix
+  #cimx = cached inverted matrix
+  #rmx = returned matrix
+  
   #is there a cached matrix?
-  
-  #if so, does it match the current matrix?
-  
-    #if so, return the cached inverse
-  
+      if(exists(cmx)) {
+            
+  #if so, 
+      #does it match the current matrix?
+      if(cmx==x) {
+      #if so, 
+      #return the cached inverse
+      rmx <- cimx
+    }
     #if not, 
-      #calculate the inverse
+    else {
+      #set flag
+      needtocalculate == TRUE
+    }
+  }
   
-      #cache the inverse
+  #if not,
+  else {
+      #set flag
+      needtocalculate == TRUE
+      #cache the matrix
+      cmx <- pmx
+  }
   
-  #if not, cache provided matrix
+  if (needtocalculate) {
+    
+    #calculate the inverse
+    rmx <- solve(pmx)
+    
+    #cache the inverse
+    cmx <<- rmx
+    
+  }
   
   #return the inverse
   rmx
