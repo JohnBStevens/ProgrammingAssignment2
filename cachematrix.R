b08c14986fc2d484a1a7e3a35d0759c150e8919c
John Stevens <JohnStevens@JohnStevens.info>
2016-11-07 19:15:36 -0500

    Initial comments-only version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates an invertable matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  #generate the matrix
+  mx <- maxtrix(rnorm(16),nrow=10,ncol=10)
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the given matrix, 
+## calculating the inverse if it is not already available in the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  #is there a cached matrix?
+  
+  #if so, does it match the current matrix?
+  
+    #if so, return the cached inverse
+  
+    #if not, 
+      #calculate the inverse
+  
+      #cache the inverse
+  
+  #if not, cache provided matrix
+  
+  #return the inverse
+  rmx
 }
